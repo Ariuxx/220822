1d1749bf8816303f481255f2c2a6ac316590a57a
Ariuxx <118547519+Ariuxx@users.noreply.github.com>
2023-03-30 22:50:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const yesbtn= document.querySelector('#yesbtn');
 //yesbtn.style.setPropety('Back')
 
 yesbtn.addEventListener('click',function () {
-    alert('Lo sabía :).' /n 'Creo que no lo sabes pero ese 22 de diciembre se quedo guardado en mi alma. ')
+    alert('Lo sabía :). Creo que no lo sabes pero ese 22 de diciembre se quedo guardado en mi alma. ')
 
 });
 
